562d8c0ae6b6d6664a516efba061d9fc5c0a1a7e
anshadmohdmp <anshadmohdmp@gmail.com>
2026-02-07 01:19:58 +0400

    changed

--- Admin/src/ForgetPassword.jsx
+++ Admin/src/ForgetPassword.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Card, Form, Button } from "react-bootstrap";
+import "./Css/ForgetPassword.css"
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
@@ -26,8 +27,19 @@ const ForgotPassword = () => {
     }
   };
 
+  const inputStyle = {
+    border: "none",
+    borderRadius: "10px",
+    padding: "12px",
+    color: "#fff",
+    background: "#3a3a3a",
+    boxShadow:
+      "inset 3px 3px 6px rgba(0,0,0,0.6), inset -3px -3px 6px rgba(255,255,255,0.05)",
+    cursor:"pointer"
+  };
+
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
+    <div className="d-flex justify-content-center align-items-center vh-100 forgetpass">
       <Card style={{ width: "400px", padding: "30px", background: "#2a2a2a", color: "#fff" }}>
         <h2 style={{ textAlign: "center", marginBottom: "20px" }}>Forgot Password</h2>
         <Form onSubmit={handleSubmit}>
@@ -38,13 +50,14 @@ const ForgotPassword = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder="Enter your registered email"
+              style={inputStyle}
             />
           </Form.Group>
 
           {error && <p style={{ color: "red", textAlign: "center" }}>{error}</p>}
           {message && <p style={{ color: "green", textAlign: "center" }}>{message}</p>}
 
-          <Button type="submit" className="w-100" style={{ background: "#1a37aa", border: "none" }}>
+          <Button type="submit" className="w-100" style={{ background: "#c0941b", border: "none" }}>
             Send Reset Link
           </Button>
         </Form>
