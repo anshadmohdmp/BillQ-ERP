c1d317c2dd4646f4d3af45dccbcdbc2d43925d9e
anshadmohdmp <anshadmohdmp@gmail.com>
2026-02-12 20:49:14 +0400

    changed

--- Admin/src/ProtectedRoute.jsx
+++ Admin/src/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "./AuthProvider";
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
